a46b3d00e2276e51afe51a33244839ddbffe7576
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2024-05-14 13:04:08 +0100

    Animation || How

--- js/main.js
+++ js/main.js
@@ -34,14 +34,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	gsap.registerPlugin(ScrollTrigger);
 	animateHeader();
 	animateHero();
+	animateAbout();
+	animateHow();
 });
-// End Animations
-let load = gsap.timeline({
-	defaults: {
-		duration: 1.5,
-		ease: "power4.inOut",
-	},
-});
+const slideDown = {
+	opacity: 0,
+	y: -50,
+};
+const slideDownStager = {
+	opacity: 0,
+	y: -50,
+	stagger: 0.3,
+};
+const slideLeft = {
+	opacity: 0,
+	x: 1500,
+};
+const slideRight = {
+	opacity: 0,
+	x: -1500,
+};
 function animateHeader() {
 	let load = gsap.timeline({
 		defaults: {
@@ -50,9 +62,7 @@ function animateHeader() {
 		},
 	});
 	load
-		.from("header .navbar-brand", {
-			x: 1500,
-		})
+		.from("header .navbar-brand", slideLeft)
 		.from("header .navigation li", {
 			y: -200,
 			stagger: 0.15,
@@ -70,18 +80,45 @@ function animateHero() {
 		},
 	});
 	load
-		.from(".hero .text-copy > h1, .hero .text-copy > p", {
-			opacity: 0,
-			y: -50,
-			stagger: 0.3,
-		})
+		.from(".hero .text-copy > h1, .hero .text-copy > p", slideDownStager)
 		.from(".hero .text-copy .badges a", {
-			opacity: 0,
-			x: 1500,
+			...slideLeft,
 			stagger: 0.3,
 		})
 		.from(
 			".hero .column-two, .hero .column-one",
+			{
+				...slideRight,
+				stagger: 0.3,
+			},
+			"<"
+		)
+		.from(".hero .text-copy .features .feature", slideDownStager)
+		.from(".hero .scroll-button", {
+			opacity: 0,
+			y: 150,
+		});
+}
+function animateAbout() {
+	let load = gsap.timeline({
+		scrollTrigger: {
+			trigger: "section.about",
+		},
+		defaults: {
+			duration: 1.5,
+			ease: "power4.inOut",
+		},
+	});
+	load
+		.from("section.about .section-question", {
+			x: 1500,
+		})
+		.from("section.about h2", {
+			opacity: 0,
+			y: -50,
+		})
+		.from(
+			"section.about img",
 			{
 				opacity: 0,
 				x: -1500,
@@ -89,13 +126,38 @@ function animateHero() {
 			},
 			"<"
 		)
-		.from(".hero .text-copy .features .feature", {
+		.from("section.about ul li", {
 			opacity: 0,
 			y: -50,
 			stagger: 0.3,
 		})
-		.from(".hero .scroll-button", {
-			opacity: 0,
-			y: 150,
-		});
+		.from(
+			"section.about .stats, section.about .decoration",
+			{
+				opacity: 0,
+				x: -1500,
+			},
+			"<"
+		);
 }
+function animateHow() {
+	const section = ".how";
+	let load = gsap.timeline({
+		scrollTrigger: {
+			trigger: section,
+		},
+		defaults: {
+			duration: 1.5,
+			ease: "power4.inOut",
+		},
+	});
+	load
+		.from(`${section} .section-question`, slideDown)
+		.from(`${section} h2`, slideDown)
+		.from(`${section} img`, {
+			scale: 0,
+			duration: 0.65,
+		})
+		.from(`${section} .step`, slideDownStager);
+}
+// End Animations
